da223cdb5d0b32de10b077e7191143bce7cdf2f9
Limchakyoung <ckrud8614@naver.com>
2020-06-25 18:25:22 +0900

    ui소스파일 업로드

---
+++ app/src/main/java/com/example/localcurrency4/ClickedItemActivity.java
@@ -0,0 +1,86 @@
+package com.example.localcurrency4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ClickedItemActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textView;
+
+    //리스트에 나오는 메뉴들
+    final static String[] LIST_MENU1 = {"농.축.수산물 업소1", "농.축.수산물 업소2", "농.축.수산물 업소3","농.축.수산물 업소4","농.축.수산물 업소5"};
+    final static String[] LIST_MENU2 = {"슈퍼 업소1", "슈퍼 업소2", "슈퍼 업소3","슈퍼 업소4","슈퍼 업소5"};
+    final static String[] LIST_MENU3 = {"매점 업소1", "매점 업소2", "매점 업소3","매점 업소4","매점 업소5"};
+    final static String[] LIST_MENU4 = {"음료/디저트", "주류/주점", "한식","중식","일식","양식","반찬/도시락","야식","기타음식","분식","뷔페","그 외 아시아음식점"};
+    final static String[] LIST_MENU5 = {"건강식품 업소1", "건강식품 업소2", "건강식품 업소3","건강식품 업소4","건강식품 업소5"};
+    final static String[] LIST_MENU6 = {"유통급식업 업소1", "유통급식업 업소2", "유통급식업 업소3","유통급식업 업소4","유통급식업 업소5"};
+
+    String[] list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clicked_item);
+
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.tvName);
+
+        //상위카테고리에서 하나 클릭하면 전환되는 화면
+        Intent intent = getIntent();
+        if (intent.getExtras() != null) {
+            String selectedName = intent.getStringExtra("name");
+            int selectedImage = intent.getIntExtra("image", 0);
+
+            textView.setText(selectedName);
+            imageView.setImageResource(selectedImage);
+
+            switch(selectedName) {
+                case "농.축.수산물":
+                    list = LIST_MENU1;
+                    break;
+                case "슈퍼":
+                    list = LIST_MENU2;
+                    break;
+                case "매점":
+                    list = LIST_MENU3;
+                    break;
+                case "음식점업":
+                    list = LIST_MENU4;
+                    break;
+                case "건강식품":
+                    list = LIST_MENU5;
+                    break;
+                case "유통급식업":
+                    list = LIST_MENU6;
+                    break;
+            }
+
+        }
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+
+        ListView listview = (ListView) findViewById(R.id.listview);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                String strText = (String) parent.getItemAtPosition(position);
+
+                //아이콘 클릭하면 지도로 넘어감
+                Intent mapIntent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(mapIntent);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/localcurrency4/MainActivity.java
@@ -0,0 +1,140 @@
+package com.example.localcurrency4;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    GridView gridView;//(BaseAdapter 상속받은) 어댑터를 그리드 뷰에 적용
+
+    String[] names = {"농.축.수산물", "슈퍼", "매점", "음식점업", "건강식품", "유통급식업"};
+    int[] images = {R.drawable.item1, R.drawable.item2, R.drawable.item3, R.drawable.item4, R.drawable.item5, R.drawable.item6};
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_action, menu) ;
+
+        return true ;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gridView = findViewById(R.id.gridView);
+
+        //커스텀 어댑터
+        CustomAdapter customAdapter = new CustomAdapter(names, images, this);
+
+        gridView.setAdapter(customAdapter);//어댑터를 그리드 뷰에 적용
+
+        //그리드 뷰 아이템 클릭하면
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                 /*
+                String selectedName = names[i];
+                int selectedImage = images[i];
+
+                //putExtra : 다른 Activity의 데이터 전달 , getExtra 로 데이터 전달받음
+                startActivity(new Intent(MainActivity.this,ClickedItemActivity.class).putExtra("name",selectedName).putExtra("image",selectedImage));
+
+                if(i != 3){
+                    //아이콘 클릭하면 지도로 넘어감
+                    Intent mapIntent = new Intent(getApplicationContext(), MapsActivity.class);
+                    startActivity(mapIntent);
+                }
+
+                  */
+                 if(i == 3){
+                     String selectedName = names[i];
+                     int selectedImage = images[i];
+
+                     //putExtra : 다른 Activity의 데이터 전달 , getExtra 로 데이터 전달받음
+                     startActivity(new Intent(MainActivity.this,ClickedItemActivity.class).putExtra("name",selectedName).putExtra("image",selectedImage));
+                 }
+                 else{
+                     //아이콘 클릭하면 지도로 넘어감
+                     Intent mapIntent = new Intent(getApplicationContext(), MapsActivity.class);
+                     startActivity(mapIntent);
+                 }
+            }
+        });
+    }
+
+    public class CustomAdapter extends BaseAdapter{
+        private String[] imageNames;
+        private int[] imagesPhoto;
+        private Context context;
+        private LayoutInflater layoutInflater; //view를 만듦
+
+        public CustomAdapter(String[] imageNames, int[] imagesPhoto, Context context) {
+            this.imageNames = imageNames;
+            this.imagesPhoto = imagesPhoto;
+            this.context = context;
+            this.layoutInflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return imagesPhoto.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+            if(view == null){
+                view = layoutInflater.inflate(R.layout.row_items, viewGroup, false);
+
+            }
+
+            TextView tvName = view.findViewById(R.id.tvName);
+            ImageView imageView = view.findViewById(R.id.imageView);
+
+            tvName.setText(imageNames[i]);
+            imageView.setImageResource(imagesPhoto[i]);
+
+            return view;
+
+        }
+    }
+
+    public class AddActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);//뒤로가기 버튼 생성
+
+
+        }
+    }
+}
