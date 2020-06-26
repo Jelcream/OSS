371b8082fa6219a4fcd8fdcaa07c02267092720d
jelcream <chrysos38@gmail.com>
2020-06-26 19:24:50 +0900

    완성본

--- app/src/main/java/tm/project/locals/MapsActivity.java
+++ app/src/main/java/tm/project/locals/MapsActivity.java
@@ -5,10 +5,11 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -28,9 +29,12 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 
+
+
 import java.util.ArrayList;
 
 import static android.Manifest.permission;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -69,11 +73,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
-
+        makerEvent();
         //위 경도 나타내는 객체
         LatLng suwonStation = new LatLng(37.267328, 126.999365);
         //위치에 포지션을 하고
-        //mMap.addMarker(new MarkerOptions().position(suwonStation).title("수원역"));
         //그 위치로 카메라를 움직임
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(suwonStation, 16.0f));
 
@@ -82,13 +85,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (ActivityCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION) != PackageManager
                 .PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, permission
                 .ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             //권한이 없으면 permission 요청->다이얼로그가 뜰 것임 , 사용자가 권한 승낙 거부 할 수 있음
             ActivityCompat.requestPermissions(this,
                     new String[]{permission.ACCESS_FINE_LOCATION, permission
@@ -118,53 +114,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             }
         });
-        //분류에 따라 음식점 위치 마커 뿌리는 메소드 호출하기
-        //데이터 확인하고 이름이 같은 데이터 데려오기
     }
-
-    //버튼 삭제
-    /*
-    //버튼 눌렀을 때 현재위치를 나타내기-1
-    public void onLastLocationClicked(View view) {
-        //현재위치 등의 기능 사용직전에 체크를 해줘야 함
-        if (ActivityCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION) != PackageManager
-                .PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, permission
-                .ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            //권한이 없으면 permission 요청->다이얼로그가 뜰 것임 , 사용자가 권한 승낙 거부 할 수 있음
-            ActivityCompat.requestPermissions(this,
-                    new String[] {permission.ACCESS_FINE_LOCATION, permission
-                            .ACCESS_COARSE_LOCATION}, REQUEST_CODE_PERMISSIONS);
-            return;
-        }
-        //권한이 있으면 이 메소드 호출-good
-        //mfusedLocationClient에서 마지막 위치를 가져오는 것을 성공하면 OnSuccessListener가 동작하면
-        mfusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            //이 메소드 호출
-            public void onSuccess(Location location) {
-                //location이 null일 수 있으니 null 체크는 해줘야 함
-                if(location != null){
-                    //location에 위도와 경도가 들어있다.
-                    LatLng mylocation = new LatLng(location.getLatitude(), location.getLongitude());//위도 경도 잡아주고
-                    mMap.addMarker(new MarkerOptions()
-                    .position(mylocation)
-                    .title("현재 위치"));//위도 경도를 맵에 표시
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(mylocation));
-                    mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
-                }
-            }
-        });
-        //분류에 따라 음식점 위치 마커 뿌리는 메소드 호출하기
-        //데이터 확인하고 이름이 같은 데이터 데려오기
-    }
-     */
     //사용자 요청 처리, 권한요청수락하면 여기서 받아서 다시 체크-2
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -199,29 +149,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void settingMark() {
-        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
-            @Override
-            public void onInfoWindowClick(Marker marker) {
-
-                Intent intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:01039096030"));
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                }
-            }
-        });
         double distance;
         //다중 마커
         //Log.v("settingMark","checking mark");
-        mMap.clear();
         if(index == 2) Log.i("#########", LS[index].get(1).getCategory1());
         for (int i = 0; i < LS[index].size(); i++) {
             distance = mlocation.distanceTo(LS[index].get(i).getLocal());
-            Log.v("@@@@","거리 구하기 "+distance);
             if(distance/1000 <= 3 && (index != 2 || LS[index].get(i).getCategory2().equals(ClickedItemActivity.LIST_MENU4[index2]))) {
                 MarkerOptions makerOptions = new MarkerOptions();
                 makerOptions.position(LS[index].get(i).getLocation()).title(LS[index].get(i).getName());
-                makerOptions.snippet(LS[index].get(i).getCategory2());
+                makerOptions.snippet(LS[index].get(i).getAddress1());
                 if(LS[index].get(i).getExemplary()) makerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
                 if(LS[index].get(i).getKind()) makerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
                 mMap.addMarker(makerOptions);
@@ -235,11 +172,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
             @Override
             public void onInfoWindowClick(Marker marker) {
+                String strs;
+                for(LocalShop localx : LS[index]){
+                    Log.v("local test",localx.getName() +" "+index+" "+index2 + " "+LS[index].size());
+                    Log.v("local latLon", Double.toString(localx.getLatitude())+ " "+ Double.toString(localx.getLongitude()));
+                    Log.v("Marker", Double.toString(marker.getPosition().latitude)+" "+Double.toString(marker.getPosition().longitude));
+                    //marker.getTitle().equals(localx.getName())
+                    // (index != 2 || localx.getCategory2().equals(ClickedItemActivity.LIST_MENU4[index2]))
+                    if(localx.getLocation().latitude == marker.getPosition().latitude &&localx.getLocation().longitude == marker.getPosition().longitude
+                        && localx.getName().equals(marker.getTitle())){
+                        String ganun = "불가능";
+                        String park = "불가능";
+                        if(localx.getDelivery()) ganun = "가능";
+                        if(localx.getParking())park = "가능";
+                        strs =  " 업소명: " + localx.getName()+"\n\n 주소: "+localx.getAddress1()+"\n\n 연락처: "+ localx.getPhoneNumber()
+                                +"\n\n 분류: "+localx.getCategory1()+" - " +localx.getCategory2();
+                        if(localx.getExemplary()) strs = strs+ "\n\n 모범 음식점 주메뉴: " + localx.getMain_menu();
+                        if(localx.getKind()) strs = strs+"\n\n 착한 가격 음식점 추천 메뉴: "+ localx.getRepItem() +"\n\n 착한 가격: "+localx.getChange()+"\n\n 영업 시간: " + localx.getDetail()
+                                +"\n\n 배달 가능 여부: "+ganun+"\n\n 주차 가능 여부: "+park;
+                        TextView text2 = findViewById(R.id.nameText);
+                        text2.setText(localx.getName());
+                        TextView text3 = findViewById(R.id.listView2);
+                        text3.setText(strs);
+                        break;
+                    }
+                }
 
             }
         });
     }
-
+    //, "모범 음식점 주 메뉴: "ls.getMain_menu(),
+    //                "착한 가격 업소 추천 메뉴: "+ls.getRepItem(),
     public void GPSCheck() {
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             //GPS 설정화면으로 이동
